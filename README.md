14072763b3637cfc25577ffbffb6aade0eba8456
Xiang Wang <gnaixgnaw@gmail.com>
2013-04-25 10:51:46 -0400

    code cleanup

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 CSP
 ===
 
-Constrained Spectral Clustering with Active Extension
+Spectral clustering for complex graphs, including:
++ Constrained spectral clustering
++ Active spectral clustering
++ Multi-view spectral clustering.
 
 ###Author###
-Xiang Wang, UC Davis
+Xiang Wang
 
 ###Website###
 https://sites.google.com/site/gnaixgnaw
 
 ###Citation###
-+ Xiang Wang, Ian Davidson. Flexible constrained spectral clustering. In KDD 2010, pp. 563-572. 
-+ Xiang Wang, Buyue Qian, Ian Davidson. On constrained spectral clustering and its applications. Data Min. Knowl. Discov., in press, 2012.
-+ Xiang Wang, Ian Davidson. Active spectral clustering. In ICDM 2010, pp. 561-568.
++ [constrained]Xiang Wang, Ian Davidson. Flexible constrained spectral clustering. In KDD 2010, pp. 563-572. 
++ [constrained]Xiang Wang, Buyue Qian, Ian Davidson. On constrained spectral clustering and its applications. Data Min. Knowl. Discov., in press, 2012.
++ [active]Xiang Wang, Ian Davidson. Active spectral clustering. In ICDM 2010, pp. 561-568.
++ [multi-view]Xiang Wang, Buyue Qian, Jieping Ye, Ian Davidson. Multi-objective multi-view spectral clustering via Pareto optimization. In SDM 2013, to appear.
 
 ###Environment###
 Matlab R2009b or later
+
+###Usage###
+Run the demo_* files to start.
--- load_wine.m
+++ load_wine.m
@@ -1,40 +1,62 @@
 % Load the data set, trim it into two classes.
-tmp=load('wine.data');
+tmp = load('wine.data');
+label = tmp(:,1);
+data = tmp(label~=1,2:end);
+label(label==1) = [];
+clear tmp;
 
-% Create labels, '1' for one class and '-1' for the other.
-label=tmp(:,1);
-label(label==2)=1;
-label(label==3)=-1;
+% Convert labels, '1' for one class and '-1' for the other.
+label(label==2) = 1;
+label(label==3) = -1;
+
+% The complete constraint matrix
+Q_star = label*label';
 
 % Center and normalize the data attributes. Each row is an instace and
 % each column is an attribute.
-data=tmp(:,2:end);
-clear tmp;
-for i=1:size(data,2)
-    data(:,i)=data(:,i)-mean(data(:,i));
+for i = 1:size(data,2)
+    data(:,i) = data(:,i) - mean(data(:,i));
 end
-my_var=var(data);
+my_var = var(data);
 % The size of the data set
-N=size(data,1);
+N = size(data,1);
 
 % Compute the similarity matrix A using RBF kernel.
-A=zeros(N,N);
-for i=1:N
-    for j=1:N
-        A(i,j)=exp(-1*sum((data(i,:)-data(j,:)).^2./(2*my_var)));
+% Set diagonal entries to 0 for numerical consideration.
+A = zeros(N,N);
+for i = 1:N
+    for j=(i+1):N
+        A(i,j) = exp(-1*sum((data(i,:)-data(j,:)).^2./(2*my_var)));
+        A(j,i) = A(i,j);
     end
 end
 
-% Set diagonal entries to 0 for numerical consideration.
-for i=1:N
-    A(i,i)=0;
-end
-
 % Compute the graph Laplacian.
-D=diag(sum(A));
-vol=sum(diag(D));
-D_norm=D^(-1/2);
-L=eye(N)-D_norm*A*D_norm;
+D = diag(sum(A)); vol = sum(diag(D)); D_norm = D^(-1/2);
+L = eye(N) - D_norm*A*D_norm;
 
-% The complete constraint matrix
-Q_star=label*label';
+% Construct View 1 for multi-view learning
+data1 = data(:,1:6);
+my_var1 = var(data1);
+A1 = zeros(N,N);
+for i = 1:N
+    for j = (i+1):N
+        A1(i,j) = exp(-1*sum((data1(i,:)-data1(j,:)).^2./(2*my_var1)));
+        A1(j,i) = A1(i,j);
+    end
+end
+D1 = diag(sum(A1)); vol1 = sum(diag(D1)); D_norm1 = D1^(-1/2);
+L1 = eye(N) - D_norm1*A1*D_norm1;
+
+% Construct View 2 for multi-view learning
+data2 = data(:,7:end);
+my_var2 = var(data2);
+A2 = zeros(N,N);
+for i = 1:N
+    for j = (i+1):N
+        A2(i,j) = exp(-1*sum((data2(i,:)-data2(j,:)).^2./(2*my_var2)));
+        A2(j,i) = A2(i,j);
+    end
+end
+D2 = diag(sum(A2)); vol2 = sum(diag(D2)); D_norm2 = D2^(-1/2);
+L2 = eye(N) - D_norm2*A2*D_norm2;
